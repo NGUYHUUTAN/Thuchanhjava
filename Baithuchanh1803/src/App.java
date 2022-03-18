398c9c10d7e92847c8a02fd1c33adbe868361536
NGUYHUUTAN <nguytan2002345@gmail.com>
2022-03-18 22:09:32 +0700

    commit

---
+++ Baithuchanh1803/src/App.java
@@ -0,0 +1,25 @@
+public class App {import Main.HinhChuNhat;
+    import Main.HinhTron;
+    
+    public class App {
+        public static void main(String[] args) {
+       HinhTron hinhTron = new HinhTron();
+       HinhChuNhat hinhChuNhat = new HinhChuNhat();
+       hinhTron.nhapbankinh();
+       hinhTron.tinhchuvi();
+       hinhTron.tinhdientich();
+       hinhTron.inchuvi();
+       hinhTron.indientich();
+       System.out.println("\n\n");
+       hinhChuNhat.nhapchieudai();
+       hinhChuNhat.nhapchieurong();
+       hinhChuNhat.tinhchuvi();
+       hinhChuNhat.tinhdientich();
+       hinhChuNhat.inchuvi();
+       hinhChuNhat.indientich();
+       }
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
